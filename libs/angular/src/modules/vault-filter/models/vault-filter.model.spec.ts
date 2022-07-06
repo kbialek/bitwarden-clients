2a5d6ee32c87f95da80703b07719d98304aa2a53
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-07-06 11:09:58 +0200

    chore: group and reword tests for better readability

--- libs/angular/src/modules/vault-filter/models/vault-filter.model.spec.ts
+++ libs/angular/src/modules/vault-filter/models/vault-filter.model.spec.ts
@@ -5,184 +5,203 @@ import { VaultFilter } from "./vault-filter.model";
 
 describe("VaultFilter", () => {
   describe("filterFunction", () => {
-    it("should return true when filter is set to all statuses", () => {
-      const cipher = createCipher();
-      const filterFunction = createFilterFunction({ status: "all" });
+    describe("generic cipher", () => {
+      it("should return true when not filtering for anything specific", () => {
+        const cipher = createCipher();
+        const filterFunction = createFilterFunction({ status: "all" });
 
-      const result = filterFunction(cipher);
+        const result = filterFunction(cipher);
 
-      expect(result).toBe(true);
+        expect(result).toBe(true);
+      });
     });
 
-    it("should return true when filter is set to favorites and cipher is favorite", () => {
+    describe("favorite cipher", () => {
       const cipher = createCipher({ favorite: true });
-      const filterFunction = createFilterFunction({ status: "favorites" });
 
-      const result = filterFunction(cipher);
+      it("should return true when filtering for favorites", () => {
+        const filterFunction = createFilterFunction({ status: "favorites" });
 
-      expect(result).toBe(true);
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(true);
+      });
+
+      it("should return false when filtering for trash", () => {
+        const filterFunction = createFilterFunction({ status: "trash" });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(false);
+      });
     });
 
-    it("should return false when filter is set to all and cipher is not favorite", () => {
-      const cipher = createCipher({ favorite: false });
-      const filterFunction = createFilterFunction({ status: "favorites" });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(false);
-    });
-
-    it("should return true when filter is set to trash and cipher is deleted", () => {
+    describe("deleted cipher", () => {
       const cipher = createCipher({ deletedDate: new Date() });
-      const filterFunction = createFilterFunction({ status: "trash" });
 
-      const result = filterFunction(cipher);
+      it("should return true when filtering for trash", () => {
+        const filterFunction = createFilterFunction({ status: "trash" });
 
-      expect(result).toBe(true);
-    });
+        const result = filterFunction(cipher);
 
-    it("should return false when filter is set to trash and cipher is not deleted", () => {
-      const cipher = createCipher({ deletedDate: undefined });
-      const filterFunction = createFilterFunction({ status: "trash" });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(false);
-    });
-
-    it("should return true when filter matches cipher type", () => {
-      const cipher = createCipher({ type: CipherType.Identity });
-      const filterFunction = createFilterFunction({ cipherType: CipherType.Identity });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(true);
-    });
-
-    it("should return false when filter does not match cipher type", () => {
-      const cipher = createCipher({ type: CipherType.Card });
-      const filterFunction = createFilterFunction({ cipherType: CipherType.Identity });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(false);
-    });
-
-    it("should return true when filter matches folder id", () => {
-      const cipher = createCipher({ folderId: "folderId" });
-      const filterFunction = createFilterFunction({
-        selectedFolder: true,
-        selectedFolderId: "folderId",
+        expect(result).toBe(true);
       });
 
-      const result = filterFunction(cipher);
+      it("should return false when filtering for favorites", () => {
+        const filterFunction = createFilterFunction({ status: "favorites" });
 
-      expect(result).toBe(true);
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(false);
+      });
     });
 
-    it("should return true when filtering on unassigned folder and cipher does not have folder", () => {
+    describe("cipher with type", () => {
+      it("should return true when filter matches cipher type", () => {
+        const cipher = createCipher({ type: CipherType.Identity });
+        const filterFunction = createFilterFunction({ cipherType: CipherType.Identity });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(true);
+      });
+
+      it("should return false when filter does not match cipher type", () => {
+        const cipher = createCipher({ type: CipherType.Card });
+        const filterFunction = createFilterFunction({ cipherType: CipherType.Identity });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(false);
+      });
+    });
+
+    describe("cipher with folder id", () => {
+      it("should return true when filter matches folder id", () => {
+        const cipher = createCipher({ folderId: "folderId" });
+        const filterFunction = createFilterFunction({
+          selectedFolder: true,
+          selectedFolderId: "folderId",
+        });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(true);
+      });
+
+      it("should return false when filter does not match folder id", () => {
+        const cipher = createCipher({ folderId: "folderId" });
+        const filterFunction = createFilterFunction({
+          selectedFolder: true,
+          selectedFolderId: "anotherFolderId",
+        });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(false);
+      });
+    });
+
+    describe("cipher without folder", () => {
       const cipher = createCipher({ folderId: undefined });
-      const filterFunction = createFilterFunction({
-        selectedFolder: true,
-        selectedFolderId: null,
+
+      it("should return true when filtering on unassigned folder", () => {
+        const filterFunction = createFilterFunction({
+          selectedFolder: true,
+          selectedFolderId: null,
+        });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(true);
       });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(true);
     });
 
-    it("should return false when filter does not match folder id", () => {
-      const cipher = createCipher({ folderId: "folderId" });
-      const filterFunction = createFilterFunction({
-        selectedFolder: true,
-        selectedFolderId: "anotherFolderId",
-      });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(false);
-    });
-
-    it("should return true when filter matches collection id", () => {
-      const cipher = createCipher({ collectionIds: ["collectionId", "anotherId"] });
-      const filterFunction = createFilterFunction({ selectedCollectionId: "collectionId" });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(true);
-    });
-
-    it("should return true when filtering on unassigned collection and cipher belongs to organization and does not have any collections", () => {
-      const cipher = createCipher({ organizationId: "organizationId", collectionIds: [] });
-      const filterFunction = createFilterFunction({
-        selectedCollection: true,
-        selectedCollectionId: null,
-      });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(true);
-    });
-
-    it("should return false when filtering on unassigned collection but cipher does not have organization", () => {
-      const cipher = createCipher({ organizationId: null, collectionIds: [] });
-      const filterFunction = createFilterFunction({
-        selectedCollection: true,
-        selectedCollectionId: null,
-      });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(false);
-    });
-
-    it("should return false when filter does not match collection id", () => {
+    describe("organizational cipher (with organization and collections)", () => {
       const cipher = createCipher({
         organizationId: "organizationId",
         collectionIds: ["collectionId", "anotherId"],
       });
-      const filterFunction = createFilterFunction({
-        selectedCollection: true,
-        selectedCollectionId: "nonMatchingId",
+
+      it("should return true when filter matches collection id", () => {
+        const filterFunction = createFilterFunction({ selectedCollectionId: "collectionId" });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(true);
       });
 
-      const result = filterFunction(cipher);
+      it("should return false when filter does not match collection id", () => {
+        const filterFunction = createFilterFunction({
+          selectedCollection: true,
+          selectedCollectionId: "nonMatchingId",
+        });
 
-      expect(result).toBe(false);
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(false);
+      });
+
+      it("should return false when filter does not match organization id", () => {
+        const filterFunction = createFilterFunction({
+          selectedOrganizationId: "anotherOrganizationId",
+        });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(false);
+      });
     });
 
-    it("should return true when filter matches organization id", () => {
-      const cipher = createCipher({ organizationId: "organizationId" });
-      const filterFunction = createFilterFunction({
-        selectedOrganizationId: "organizationId",
+    describe("unassigned organizational cipher (with organization, without collection)", () => {
+      const cipher = createCipher({ organizationId: "organizationId", collectionIds: [] });
+
+      it("should return true when filtering for unassigned collection", () => {
+        const filterFunction = createFilterFunction({
+          selectedCollection: true,
+          selectedCollectionId: null,
+        });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(true);
       });
 
-      const result = filterFunction(cipher);
+      it("should return true when filter matches organization id", () => {
+        const filterFunction = createFilterFunction({
+          selectedOrganizationId: "organizationId",
+        });
 
-      expect(result).toBe(true);
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(true);
+      });
     });
 
-    it("should return true when filtering on my vault only and cipher does not have any organization", () => {
-      const cipher = createCipher({ organizationId: null });
-      const filterFunction = createFilterFunction({
-        myVaultOnly: true,
+    describe("individual cipher (without organization or collection)", () => {
+      const cipher = createCipher({ organizationId: null, collectionIds: [] });
+
+      it("should return false when filtering for unassigned collection", () => {
+        const filterFunction = createFilterFunction({
+          selectedCollection: true,
+          selectedCollectionId: null,
+        });
+
+        const result = filterFunction(cipher);
+
+        expect(result).toBe(false);
       });
 
-      const result = filterFunction(cipher);
+      it("should return true when filtering for my vault only", () => {
+        const cipher = createCipher({ organizationId: null });
+        const filterFunction = createFilterFunction({
+          myVaultOnly: true,
+        });
 
-      expect(result).toBe(true);
-    });
+        const result = filterFunction(cipher);
 
-    it("should return false when filter does not match organization id", () => {
-      const cipher = createCipher({ organizationId: "organizationId" });
-      const filterFunction = createFilterFunction({
-        selectedOrganizationId: "anotherOrganizationId",
+        expect(result).toBe(true);
       });
-
-      const result = filterFunction(cipher);
-
-      expect(result).toBe(false);
     });
   });
 });
